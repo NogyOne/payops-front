74f9c5e89aa3543b7dc4fc7f8b42a3ecc6cc0489
German <german_gomez1205@hotmail.com>
2024-04-05 23:30:25 -0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,32 @@ const API_URL = 'http://localhost:3001'
 
 export const getSubscriptions = async () => {
     try {
-        return await (await fetch(`${API_URL}/subscriptions`)).json()
+        return await (
+            await fetch(`${API_URL}/subscriptions`)).json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getCustomers = async () => {
+    try {
+        return await (
+            await fetch(`${API_URL}/customers`)).json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const addCustomer = async customerObject => {
+    try {
+        return await (
+            await fetch(`${API_URL}/customers`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(customerObject)
+            })).json()
     } catch (error) {
         console.log(error)
     }
