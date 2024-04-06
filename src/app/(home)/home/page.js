5723a3de104710b7c146e3e0008d9e17991d13c0
German <german_gomez1205@hotmail.com>
2024-04-05 23:38:33 -0700

    Modify navbar

--- src/app/(home)/home/page.js
+++ src/app/(home)/home/page.js
@@ -4,37 +4,32 @@ import { Fragment } from 'react'
 export default function Home() {
   return (
     <section className='w-full overflow-hidden'>
-      <main>
-        <div className='w-full gap-4 px-10 py-4'>
-          <h1 className='text-4xl font-thin'>Home</h1>
-          {/* <hr className='border-gray-300 ' /> */}
+      <h1 className='px-10 py-4 text-4xl font-thin'>Home</h1>
+      <section>
+        <div className='flex justify-center mx-auto'>
+          <img
+            className='w-40 mt-8 animate-spin-slow'
+            src='/BlueSpiralV2.png'
+            alt='spiral-logo'
+          />
         </div>
-        <section>
-          <div className='flex justify-center mx-auto'>
-            <img
-              className='w-40 mt-8 animate-spin-slow'
-              src='/BlueSpiralV2.png'
-              alt='spiral-logo'
-            />
+        <article className='grid items-center justify-center grid-cols-3 mt-24'>
+          <div className='flex flex-col items-center text-2xl'>
+            <p className='font-thin'>Total of Subscribers</p>
+            <p className='font-bold text-[#386EF6]'>1000</p>
           </div>
-          <article className='grid items-center justify-center grid-cols-3 mt-24'>
-            <div className='flex flex-col items-center text-2xl'>
-              <p className='font-thin'>Total of Subscribers</p>
-              <p className='font-bold text-[#386EF6]'>1000</p>
-            </div>
 
-            <div className='flex flex-col items-center text-2xl'>
-              <p className='font-thin'>Current</p>
-              <p className='font-bold text-[#3DC07A]'>900</p>
-            </div>
+          <div className='flex flex-col items-center text-2xl'>
+            <p className='font-thin'>Current</p>
+            <p className='font-bold text-[#3DC07A]'>900</p>
+          </div>
 
-            <div className='flex flex-col items-center text-2xl'>
-              <p className='font-thin'>Expired</p>
-              <p className='font-bold text-[#F12222]'>100</p>
-            </div>
-          </article>
-        </section>
-      </main>
+          <div className='flex flex-col items-center text-2xl'>
+            <p className='font-thin'>Expired</p>
+            <p className='font-bold text-[#F12222]'>100</p>
+          </div>
+        </article>
+      </section>
     </section>
   )
 }
