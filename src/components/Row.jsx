77ca0e139f9cbd93e495c05a7e8f16fd856d587c
German <german_gomez1205@hotmail.com>
2024-04-05 23:38:05 -0700

    Update Row.js

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,36 +1,27 @@
 import React from 'react'
+import { getFormatDate } from '@/lib/utils'
 
 export default function Row({name, status, initialDate, endDate, daysLeft}) {
-  
-const formatDate = dateProv => {
-  const date = new Date(dateProv)
-  return date.toLocaleDateString('en-US',{
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  })
-}
 
 const getRemainingDays = () => {
-  const dt1 = new Date(initialDate).getTime()
-  const dt2 = new Date().getTime()
+  const difDates = new Date(endDate).getTime() - new Date().getTime()
 
-  const remainingDays = Math.ceil(((((dt2 - dt1)/1000)/60)/60/24))
+  const remainingDays = Math.ceil((difDates) / (1000 * 60 * 60 * 24))
   return remainingDays
 }
 
   return (
-    <tr className='bg-white border-b '>
+    <tr className='text-center bg-white border-b cursor-pointer hover:bg-gray-100'>
                 <th
                   scope='row'
-                  className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '
+                  className='px-6 py-4 font-medium text-left text-gray-900 whitespace-nowrap '
                 >
                   {name}
                 </th>
                 {/* Change status from row */}
                 <td className={'CURRENT' ? 'px-6 py-4 text-green-600' : 'px-6 py-4 text-red-600'}>{status}</td>
-                <td className='px-6 py-4'>{formatDate(initialDate)}</td>
-                <td className='px-6 py-4'>{formatDate(endDate)}</td>
+                <td className='px-6 py-4'>{getFormatDate(initialDate)}</td>
+                <td className='px-6 py-4'>{getFormatDate(endDate)}</td>
                 <td className='px-6 py-4'>{getRemainingDays()}</td>
               </tr>
   )
