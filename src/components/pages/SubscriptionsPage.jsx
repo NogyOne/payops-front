2d424260eb611e1483d8753c45c284534855398b
German <german_gomez1205@hotmail.com>
2024-04-15 14:01:52 -0700

    feat. move pages to components

---
+++ src/components/pages/SubscriptionsPage.jsx
@@ -0,0 +1,34 @@
+'use client'
+import React, { useState } from 'react'
+import SubsTable from '@/components/SubsTable'
+import ModalNewSubscription from '@/components/ModalNewSubscription'
+import { Icons } from '@/components/Icons'
+
+export default function SubscriptionsPage() {
+  const [showModal, setShowModal] = useState(false)
+  
+  const handleOpenModal = () => {
+    setShowModal(true)
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false)
+  }
+
+  return (
+    <section className='bg-[#EFEFEF] w-full overflow-hidden'>
+      <button
+        type='button'
+        className='absolute flex justify-center items-center bg-[#43DE8B] rounded-full w-14 h-14 right-10 top-24 hover:bg-[#3bc47b] text-white animate-jump z-50'
+        onClick={handleOpenModal}
+      >
+        <Icons.Plus className='w-10 h-10' />
+      </button>      
+      {showModal && <ModalNewSubscription handleCloseModal={handleCloseModal}/>}
+      <h1 className='px-10 py-4 text-4xl font-thin animate-fade-up'>Subscriptions</h1>
+      <article className='px-10 py-6'>
+        <SubsTable/>
+      </article>
+    </section>
+  )
+}
