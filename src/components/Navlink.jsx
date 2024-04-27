8cf47ff8e823437e3d04fa986e873c4aadf99b87
German <german_gomez1205@hotmail.com>
2024-04-26 19:26:36 -0700

    navlinks styles updated

--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function Navlink({ name, href, isActive, onClick}) {
+export default function Navlink({ name, href, currentPage, handleActive }) {
+  
   return (
-    <Link href={href} className='font-semibold hover:underline'>
+    <Link
+      href={href}
+      onClick={() =>{handleActive(href)}}
+      className={`font-semibold hover:bg-[#0DBCFB] rounded-t-lg px-2 pb-2 pt-1 ${currentPage === href ? 'bg-[#0DBCFB]' : ''}`}
+    >
       <p>{name}</p>
     </Link>
   )
