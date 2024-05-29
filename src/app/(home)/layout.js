8d7b8d3759ceb2216a8060273b278db902393a8f
Germán Gómez <german_gomez1205@hotmail.com>
2024-05-28 17:40:35 -0700

    feat. keep session by main layout

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -1,7 +1,19 @@
+'use client'
 import Navbar from '@/components/Navbar'
-import TitleBar from '@/components/TitleBar'
+import { useAuth } from '@/store/authStore'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 
 export default function Layout({ children }) {
+  const { setUser, clearUser } = useAuth()
+  const { data, status } = useSession()
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      setUser(data.user.user)
+    }
+  }, [status])
+
   return (
     <div className='h-screen bg-[#EFEFEF] overflow-hidden'>
       <header>
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,13 +3,16 @@ import LoginPage from '@/components/pages/LoginPage'
 import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { useAuth } from '@/store/authStore'
 
 export default function Login() {
-  const { status } = useSession()
+  const { setUser, clearUser } = useAuth()
+  const { data, status } = useSession()
   const router = useRouter()
 
   useEffect(() => {
     if (status === 'authenticated') {
+      setUser(data.user.user)
       router.push('/home')
     }
   }, [status])
